b6a264c76ae5e0a4c025d33a5c966659211490bc
Nicholas B <nicholas.mob1010@gmail.com>
2026-05-08 11:37:16 -0300

    Add event listener to show quiz on button click

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const start = document.getElementById("iniciar");
 start.addEventListener("click", () => {
   document.getElementById("quiz").style.display="block"
 });
+
+const nome = document.getElementById("nome")
