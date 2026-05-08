c34fc6fb39304aacee69b96e8a644b0c5ac38a7a
Nicholas B <nicholas.mob1010@gmail.com>
2026-05-08 16:23:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ start.addEventListener("click", () => {
     
   document.getElementById("quiz").style.display="block"
 });
+
+const resposta = document.querySelectorAll(".resposta");
+
+let atual = 1
+
+resposta.addEventListener("click", () => {
+  document.getElementById("p" + atual).style.display="none";
+  atual++;
+
+  const proxima = getElemetById("p" + atual)
+
+  if(proxima) {
+    proxima.style.display="block";
+  } else {
+    alert("fim do quiz")
+});
+
+
+
